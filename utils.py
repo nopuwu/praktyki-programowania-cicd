fbc9b8fd972baf02cdc70850f26d17c9ba90a2f6
nopuwu <mwalecki144@gmail.com>
2025-06-09 09:40:50 +0200

    Edit test_examply.py and utils.py using Black and Pylint

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
-# przykladowe funkcje utils .py
+"""Example utils.py file"""
 
-def add ( a: int , b: int) -> int:
+
+def add(a: int, b: int) -> int:
+    """Adds two integers a and b and returns the result."""
     return a + b
 
-def subtract ( a: int , b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """Subtracts b from a and returns the result."""
     return a - b
 
-def multiply ( a: int , b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b and returns the result."""
     return a * b
 
-def divide ( a: int , b: int) -> float :
+
+def divide(a: int, b: int) -> float:
+    """ "Divides a by b and returns the result."""
     return a / b
